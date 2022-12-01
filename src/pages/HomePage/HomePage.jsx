5ac1d37a906b9e625cafecdb9c02a66a6f809514
Artur Yakhin <artur.yakhin52@gmail.com>
2022-12-01 13:58:25 +0500

    mockapi!

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import DetailCard from '../../components/DetailCard/DetailCard'
 import { Layout } from '../../components/Layout'
 import s from './HomePage.module.scss'
 
@@ -7,7 +8,19 @@ export const HomePage = () => {
       nickName="asd"
       id={1}
     >
-      <div>main</div>
+      <div className={s.HomePageRoot}>
+        <DetailCard
+          userName={'name'}
+          likes={10}
+          comments={[
+            { text: 'asd', nickName: 'aaaaaa' },
+            { text: 'asd', nickName: 'aaaaaa' },
+            { text: 'asd', nickName: 'aaaaaa' },
+            { text: 'asd', nickName: 'aaaaaa' }
+          ]}
+          imgUrl="https://www.finalsitesupport.com/hc/article_attachments/360000650392/800x600.png"
+        />
+      </div>
     </Layout>
   )
 }
